2e63c2caee297ae20056f046086dfe79708368c6
XuanKha <nguyenxuankha5371@gmail.com>
2023-04-09 17:03:24 +0700

    update tailwindcss, paritals page, scss

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const { engine } = require('express-handlebars')
+const path = require('path')
+const morgan = require('morgan')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', engine({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => console.log(`listening on port 3000 at http://localhost:${port}`))
